b24c8c116add9bdfaadab36c322003519fe220c8
dynames44 <dynames.n44@gmail.com>
2025-03-24 16:47:51 +0900

    문자열 subStr

---
+++ Sting Exam/sliceSample.py
@@ -0,0 +1,15 @@
+
+str1 = "0123456789"
+slc1 = str1[1] 
+slc2 = str1[0:2] # 범위 0~1 이전 
+slc3 = str1[2:4] 
+slc4 = str1[:4] 
+slc5 = str1[4:] 
+slc6 = str1[-6:] #리버스는 마이너스 인덱스
+
+print(slc1)
+print(slc2)
+print(slc3)
+print(slc4)
+print(slc5)
+print(slc6)
